c6bf0842d512dac4a8534d35445bc96afedf5928
GaoKai <gkaigk1987@gmail.com>
2018-11-02 16:51:08 +0800

    自适应页面调试
    自适应页面调试

--- src/main/java/com/gk/controller/IndexController.java
+++ src/main/java/com/gk/controller/IndexController.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -43,6 +45,16 @@ public class IndexController {
 		message.setMsg(text);
 		disruptorManager.work(message);
 		return text;
+	}
+	
+	/**
+	 * 
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/rwd")
+	public String rwd(Model model) {
+		return "rwd";
 	}
 	
 }
---
+++ src/main/resources/static/js/rwd.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+$(function() {
+
+//	$('.menuicon').click(function() {
+//		
+//	});
+});
