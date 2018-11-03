871e45cd5ac343793da3aaa802cf24563662960a
GaoKai <gkaigk1987@gmail.com>
2018-11-03 21:39:40 +0800

    移动端菜单显示
    移动端菜单显示

--- src/main/resources/static/js/rwd.js
+++ src/main/resources/static/js/rwd.js
@@ -3,7 +3,12 @@
  */
 $(function() {
 
-//	$('.menuicon').click(function() {
-//		
-//	});
+	$('.menuicon').click(function(e) {
+//		alert(2121);
+		e.preventDefault();
+		$('.menu_container_1').toggleClass('menu_container_1-activ');
+	});
+	$('.menu').click(function() {
+		alert(111);
+	});
 });
