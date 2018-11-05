7ef71486fb1f0aca797f091c21d5291d524c27b5
GaoKai <gkaigk1987@gmail.com>
2018-11-05 16:52:42 +0800

    响应式修改
    响应式修改

--- src/main/resources/static/js/rwd.js
+++ src/main/resources/static/js/rwd.js
@@ -3,11 +3,34 @@
  */
 $(function() {
 
-	$('.menuicon').click(function(e) {
-//		alert(2121);
+	$('.menuicon .glyphicon-menu-hamburger').click(function(e) {
 		e.preventDefault();
-		$('.menu_container_1').toggleClass('menu_container_1-activ');
+		var navActive = $('.rwd-nav').hasClass('rwd-nav-active');
+		if(navActive) {
+			$('.rwd-nav').addClass('rwd-nav-unactive');
+		}
+		$('.menu_container').toggleClass('menu_container-active');
+		var flag = $('.menu_container').hasClass('menu_container-unactive');
+		$('.menu_container').removeClass('menu_container-unactive');
+		if(flag) {
+			$('.menu_container').addClass('menu_container-active');
+		}
 	});
+	
+	$('.aggs').click(function(e){
+		e.preventDefault();
+		var menuActive = $('.menu_container').hasClass('menu_container-active');
+		if(menuActive) {
+			$('.menu_container').addClass('menu_container-unactive');
+		}
+		$('.rwd-nav').toggleClass('rwd-nav-active');
+		var flag = $('.rwd-nav').hasClass('rwd-nav-unactive');
+		$('.rwd-nav').removeClass('rwd-nav-unactive');
+		if(flag) {
+			$('.rwd-nav').addClass('rwd-nav-active');
+		}
+	});
+	
 	$('.menu').click(function() {
 		alert(111);
 	});
